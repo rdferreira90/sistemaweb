412762cecd28072087aa23164a3c758c179e4f11
Rodrigo Ferreira <rdferreira90@gmail.com>
2020-02-24 20:24:50 -0300

    Project created

---
+++ src/test/java/com/rodrigo/sistemaweb/SistemawebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigo.sistemaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemawebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
